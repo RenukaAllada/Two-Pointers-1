a37ad79b367e157e4a308ab525baf9a9298ea2d3
Renuka Allada <renuka@Sureedus-Air.attlocal.net>
2025-01-14 21:43:35 -0600

    Complted 3rd problem

--- Sample.java
+++ Sample.java
@@ -82,3 +82,27 @@ class Solution {
         nums[j] = temp;
     }
 }
+
+/***********************PROBLEM-3********************/
+// Time Complexity : 0(n) n=length of height array
+// Space Complexity : 0(1)
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : no
+
+class Solution {
+    public int maxArea(int[] height) {
+        if(height==null || height.length==0){
+            return 0;
+        }
+        int left=0,right=height.length-1,maxArea=0;
+        while(left<right){
+            maxArea=Math.max(maxArea,Math.min(height[left],height[right])*(right-left));
+            if(height[left]<=height[right]){
+                left++;
+            }else{
+                right--;
+            }
+        }
+        return maxArea;
+    }
+}
